75e94ff8d849de7da5d1c839ca5e6dfa981bf3a1
gangslee <37660333+gangslee@users.noreply.github.com>
2020-03-01 06:05:08 +0900

    Delete ToDo Data From Local Storage
    Delete ToDo Data From Local Storage

--- todo.js
+++ todo.js
@@ -3,7 +3,18 @@ const toDoForm = document.querySelector(".js-toDoForm"),
     toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = 'toDos';
-const toDos = [];
+let toDos = [];
+
+function deleteToDo(event){
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos= toDos.filter(function(toDo){
+        return toDo.id !== parseInt(li.id);
+    });
+    toDos = cleanToDos;
+    saveToDos();
+}
 
 function saveToDos(){
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
@@ -14,8 +25,8 @@ function paintToDo(text){
     const delBtn = document.createElement("button");
     const span = document.createElement("span");
     const newId = toDos.length+1;
-
     delBtn.innerText = "❌";
+    delBtn.addEventListener("click", deleteToDo);
     span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
@@ -40,7 +51,6 @@ function loadToDos(){
     const loadedToDos = localStorage.getItem(TODOS_LS);
     if(loadedToDos !== null){
         const parsedToDos = JSON.parse(loadedToDos);
-        console.log(parsedToDos);
         parsedToDos.forEach(function(toDo){
             paintToDo(toDo.text);
         })
