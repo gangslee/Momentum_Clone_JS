acd6e6ab3424a3f893f3117e0208224b897dad21
gangslee <37660333+gangslee@users.noreply.github.com>
2020-03-01 09:07:50 +0900

    Complete Made & Repair Code
    Complete Made & Repair Code

--- todo.js
+++ todo.js
@@ -25,11 +25,11 @@ function paintToDo(text){
     const delBtn = document.createElement("button");
     const span = document.createElement("span");
     const newId = toDos.length+1;
-    delBtn.innerText = "❌";
+    delBtn.innerText = "X";
     delBtn.addEventListener("click", deleteToDo);
-    span.innerText = text;
-    li.appendChild(delBtn);
+    span.innerText = `${text}  `;
     li.appendChild(span);
+    li.appendChild(delBtn);
     li.id = newId;
     toDoList.appendChild(li);
     const toDoObj = {
